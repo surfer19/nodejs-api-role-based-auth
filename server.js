e7d7919f6f375432fb0c8f971f456d3c2c4afda8
Marian <marianmrva123@gmail.com>
2019-10-12 13:26:24 +0200

    Add basic dependencies

---
+++ server.js
@@ -0,0 +1,23 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorHandler = require('_helpers/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(cors());
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.send('hello world')
+})
+
+app.use(errorHandler)
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
